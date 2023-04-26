3b34302584a283a80fadbd21a720234f0bd2345e
NZTY2298 <noel.kenfack@yahoo.fr>
2023-04-26 10:50:19 +0100

    basic state

---
+++ lib/models/task.dart
@@ -0,0 +1,10 @@
+
+class Task {
+  String? description;
+  bool? complete;
+
+  Task({
+    this.complete = false,
+    this.description = '',
+  });
+}
---
+++ lib/views/plan_screen.dart
@@ -0,0 +1,58 @@
+import '../models/data_layer.dart';
+import 'package:flutter/material.dart';
+
+class PlanScreen extends StatefulWidget {
+  @override
+  State createState() => _PlanScreenState();
+}
+
+class _PlanScreenState extends State<PlanScreen> {
+  final plan = Plan();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Master Plan')),
+      body: _buildList(),
+      floatingActionButton: _buildAddTaskButton(),
+    );
+  }
+
+  Widget _buildList() {
+    return ListView.builder(
+      itemCount: plan.tasks.length,
+      itemBuilder: (context, index) =>
+          _buildTaskTile(plan.tasks[index]),
+    );
+  }
+
+  Widget _buildAddTaskButton() {
+    return FloatingActionButton(
+      child: Icon(Icons.add),
+      onPressed: () {
+        setState(() {
+          plan.tasks.add(Task());
+        });
+      },
+    );
+  }
+
+  Widget _buildTaskTile(Task task) {
+    return ListTile(
+      leading: Checkbox(
+          value: task.complete,
+          onChanged: (selected) {
+            setState(() {
+              task.complete = selected;
+            });
+          }),
+      title: TextField(
+        onChanged: (text) {
+          setState(() {
+            task.description = text;
+          });
+        },
+      ),
+    );
+  }
+}
